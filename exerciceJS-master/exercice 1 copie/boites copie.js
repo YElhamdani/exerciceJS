8095bbbcba20be118dfbfc761c7113d704facd91
Younes <48552550+YElhamdani@users.noreply.github.com>
2020-04-06 18:39:26 +0200

    Add files via upload

---
+++ exerciceJS-master/exercice 1 copie/boites copie.js
@@ -0,0 +1,29 @@
+// let fAno = ()=>{
+//     console.log("Je découvre les fonctions anonymes !");
+// }
+
+// fAno();
+
+(()=>{console.log("Je découvre les fonctions anonymes !");})();
+
+// let fAno = ()=>{
+//     console.log("Je découvre les fonctions anonymes !");
+// }
+
+// fAno();
+
+(()=>{console.log("Je découvre les fonctions anonymes !");})();
+// let fAno = ()=>{
+//     console.log("Je découvre les fonctions anonymes !");
+// }
+
+// fAno();
+
+(()=>{console.log("Je découvre les fonctions anonymes !");})();
+// let fAno = ()=>{
+//     console.log("Je découvre les fonctions anonymes !");
+// }
+
+// fAno();
+
+(()=>{console.log("Je découvre les fonctions anonymes !");})();

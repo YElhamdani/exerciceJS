234b3c37df013f9c9e4992e0c3e961c9861d68ff
Younes <48552550+YElhamdani@users.noreply.github.com>
2020-04-10 23:22:19 +0200

    Add files via upload

--- exerciceJS-master/ENTRAINEMENT/boite.js
+++ exerciceJS-master/ENTRAINEMENT/boite.js
@@ -1,3 +1,9 @@
 let x = 5;
 // condition ternaire
 [x > 3] ? [console.log("superieur")] :  [console.log("inferrieur")];
+// condition ternaire
+[x > 3] ? [console.log("superieur")] :  [console.log("inferrieur")];
+// condition ternaire
+[x > 3] ? [console.log("superieur")] :  [console.log("inferrieur")];
+
+console.log("")

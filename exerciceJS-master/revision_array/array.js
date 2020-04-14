fb85e136b834dcf59c47e70126e2f7591c7506ee
Younes <48552550+YElhamdani@users.noreply.github.com>
2020-04-14 23:46:42 +0200

    Add files via upload

--- exerciceJS-master/revision_array/array.js
+++ exerciceJS-master/revision_array/array.js
@@ -41,3 +41,23 @@ let monTableauAssociatif = {
 };
 delete(monTableauAssociatif.poste);
 // console.log(monTableauAssociatif);
+
+
+
+let monTableau = ['un', 'deux', 'trois', 'quatre'];
+monTableau.splice(1, 0, 'random', 'pie');
+// console.log(monTableau);
+
+let monTableau2D = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
+monTableau2D.splice(2, 0, ['30', '45', '70']);
+console.log(monTableau2D);
+
+let monTableauAssociatif = {
+    'prenom' : 'Mark',
+    'nom'    : 'Zuckerberg',
+    'poste'  : 'PDG de Facebook'
+};
+// console.log(monTableauAssociatif);
